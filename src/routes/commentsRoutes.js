903fdb2b88d3eec8dece31cb879a9b7ccd5f5d99
devlai23 <57195223+devlai23@users.noreply.github.com>
2026-05-31 23:43:36 -0500

    comment deletion

--- src/routes/commentsRoutes.js
+++ src/routes/commentsRoutes.js
@@ -2,14 +2,12 @@ import express from 'express';
 
 import commentsController from '../controllers/commentsController.js';
 import authMiddleware from '../middleware/authMiddleware.js';
-import requireAdmin from '../middleware/requireAdmin.js';
 
 const router = express.Router();
 
 router.delete(
   '/:id',
   authMiddleware,
-  requireAdmin,
   commentsController.deleteComment
 );
 
